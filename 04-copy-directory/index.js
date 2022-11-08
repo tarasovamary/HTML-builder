3aa7c72fb9ef76d4670cb24b90317a305aef7f6e
Mariya <mashka00@inbox.ru>
2022-11-08 16:54:06 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir () {
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, err => {
+    if(err) throw err;
+  });
+  
+  fs.promises.readdir(path.join(__dirname, 'files-copy'), {withFileTypes: true})
+  .then(files => {
+    for (const file of files) {
+      // let copyFile = path.join(__dirname, 'files-copy', file.name);
+      fs.unlink((path.join(__dirname, 'files-copy', file.name)), err => {
+        if(err) throw err;
+        console.log(`Deleted ${file.name}`);
+      });
+    }
+  });
+
+  fs.promises.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
+  .then(files => {
+  for (const file of files) {
+  // let existFile = path.join(__dirname, 'files', file.name);
+  // let copyFile = path.join(__dirname, 'files-copy', file.name);
+    if (file.isFile()) {
+        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), err => {
+        if (err) throw err; 
+        console.log(`Copied ${file.name}`);
+      });
+    }
+  }});
+};
+copyDir();
