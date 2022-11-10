e8e3c73acf9f7efd18fa469caccc35e5065bf03d
Mariya <mashka00@inbox.ru>
2022-11-10 15:21:12 +0300

    fix: add copy folder assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -23,18 +23,18 @@ async function createHTML() {
   }
   await fs.promises.writeFile(path.join(__dirname, 'project-dist/index.html'), templateFile);
   
-  async function copyDir () {
-    await fs.promises.mkdir(path.join(__dirname, 'project-dist/assets'), {recursive: true});
-    const assetsFolder = await fs.promises.readdir(path.join(__dirname, 'assets'), {withFileTypes: true})
-      for (const file of assetsFolder) {
+  async function copyDir(input, output) {
+    const assetsFolder = await fs.promises.readdir(input, { withFileTypes: true });
+    await fs.promises.mkdir(output, { recursive: true });
+    for (file of assetsFolder) {
         if (file.isDirectory()) {
-          await copyDir(path.join(path.join(__dirname, 'assets'), file.name), path.join(path.join(__dirname, 'project-dist/assets'), file.name));
+            await copyDir(path.join(input , file.name), path.join(output, file.name));
         } else {
-            await fs.promises.copyFile(path.join(path.join(__dirname, 'assets'), file.name), path.join(path.join(__dirname, 'project-dist/assets'), file.name));
+            await fs.promises.copyFile(path.join(input , file.name), path.join(output, file.name));
         }
-      }
-  };
-  copyDir();
+    }
+}
+copyDir(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist/assets'));
 }
 
 createHTML();
